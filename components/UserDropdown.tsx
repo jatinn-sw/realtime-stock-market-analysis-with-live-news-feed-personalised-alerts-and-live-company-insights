e0acca2917a9b2a987ab99b0db35dbeb74c5d01a
Jatin Kumar Sangewar <sangewarjatinkumar@gmail.com>
2026-02-03 04:03:31 +0530

    implemented full on auth logic

--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -14,15 +14,15 @@ import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { LogOut } from "lucide-react";
 import NavItems from "./NavItems";
+import { signOut } from "@/lib/actions/auth.actions";
     
-const UserDropdown = () => {
+const UserDropdown = ({ user }: { user: User }) => {
     const router = useRouter();
 
     const handleSignOut = async () => {
+        await signOut();
         router.push("/sign-in");
     }
-
-    const user = { name: 'Jatin', email: 'jsangewar@yahoo.com' };
 
     return (
         <DropdownMenu>
