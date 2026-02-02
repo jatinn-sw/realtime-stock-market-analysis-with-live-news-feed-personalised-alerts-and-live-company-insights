0edf39f0a597cf2d4e54bcf0cfd09ad53fe156aa
Jatin Kumar Sangewar <sangewarjatinkumar@gmail.com>
2026-02-02 21:01:36 +0530

    added header

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,34 +1,14 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import Header from "@/components/Header"
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-  title: "MarketSense",
-  description: "A real-time stock monitoring platform delivering live prices, personalized alerts and comprehensive company insights.",
-};
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en" className="dark">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+const Layout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <main className="min-h-screen text-gray-400">
+            <Header />
+            <div className="container py-10">
+                {children}
+            </div>
+        </main>
+    )
 }
+
+export default Layout
